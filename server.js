2c2bad5c4c21729094fb0d8b4fa35cdba9696555
Sanjeev Kumar <sanjeevksaini@yahoo.co.in>
2016-09-01 13:05:06 +0800

    Initialize Repo

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('Private aoure hit!');
+		next();
+	},
+	logger: function(req, res, next){
+		console.log(req.method + ' ' + new Date().toString() + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about',middleware.requireAuthentication,function(req, res){
+	res.send('I am an application architect');
+});
+
+app.use(express.static(__dirname + '/Public'));
+
+app.listen(port, function(){
+	console.log('Express server started on port ' + port);
+});
